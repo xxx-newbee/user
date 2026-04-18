ac834e3793f419b2dccbc72ba84a48cc72331400
xxx <huang_learn@163.com>
2026-04-18 10:30:35 +0800

    test register

--- internal/logic/register_logic.go
+++ internal/logic/register_logic.go
@@ -44,6 +44,7 @@ func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterRespon
 	if ck != true {
 		return nil, model.ErrCaptchaIncorrect
 	}
+
 	// 检查用户名唯一
 	res, err := l.svcCtx.UserModel.GetByUsernameOrEmail(in.Username)
 	if err != nil {
@@ -52,6 +53,7 @@ func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterRespon
 	if res != nil && res.ID > 0 {
 		return nil, model.ErrUserAlreadyExist
 	}
+
 	// 检查邮箱唯一
 	res, err = l.svcCtx.UserModel.GetByUsernameOrEmail(in.Email)
 	if err != nil {
